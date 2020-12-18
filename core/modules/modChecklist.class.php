9e283e9b209ff64d97c750891ad8d0716c519a0f
boudet jean pascal <68547382+atm-jpb@users.noreply.github.com>
2020-12-18 11:08:29 +0100

    Update modChecklist.class.php

--- core/modules/modChecklist.class.php
+++ core/modules/modChecklist.class.php
@@ -177,13 +177,13 @@ class modChecklist extends DolibarrModules
 		// Example:
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
 		$this->rights[$r][1] = 'Read';	// Permission label
-		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
+		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'read';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
 		$this->rights[$r][1] = 'Write';	// Permission label
-		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
+		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'write';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 
